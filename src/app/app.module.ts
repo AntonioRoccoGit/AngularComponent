e363cc9b1ad3ad6a507bbccdbea5efeb44db4f74
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-07-25 17:51:26 +0200

    added: ContainerComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './Container/container.component';
+import { NavComponent } from './Nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule
